7ca1e382c5f0e849dd1232984356010086aaf1f1
Pranav-Bobde <bobdep31@gmail.com>
2021-02-03 21:53:53 +0530

    .

---
+++ app/src/main/java/com/example/socialapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.socialapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.socialapp.Daos.PostDao
+import com.example.socialapp.models.Post
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), IPostAdapter {
+    private lateinit var adapter: PostAdapter
+    private lateinit var postDao: PostDao
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener{
+            val intent = Intent(this, CreatePostActivity::class.java)
+            startActivity(intent)
+        }
+
+        setUprecyclerView()
+    }
+
+    private fun setUprecyclerView() {
+        postDao = PostDao()
+        val postCollections = postDao.postCollection
+        val query = postCollections.orderBy("createdAt", Query.Direction.DESCENDING)
+        val recyclerViewOptions = FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java).build()
+
+        adapter = PostAdapter(recyclerViewOptions, this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+
+    override fun onLikeClicked(postId: String) {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/socialapp/models/Post.kt
@@ -0,0 +1,7 @@
+package com.example.socialapp.models
+
+data class Post(
+    val text: String = "",
+    val createdby: User = User(),
+    val createdAt: Long = 0L,
+    val likedBy: ArrayList<String> = ArrayList())
